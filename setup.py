cad361006cbc21419b850fc527e62797c89223d9
Berk Ozdogan <berk.ozdogan@firestorm.capital>
2025-03-30 19:21:36 -0400

    Initial version covering spot & futures end points

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name='amberdata_rest',
-    packages=find_packages(include=['amberdata_rest']),
+    packages=find_packages(include=['amberdata_rest', 'amberdata_rest.*']),
     version='0.1.0',
-    description='amberdata.io rest api python wrapper',
-    author='developer@github.com',
+    description='Amberdata.io REST API SDK',
+    author='technology@firestorm.capital',
     install_requires=[],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==8.2.2'],
