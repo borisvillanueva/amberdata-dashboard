9699a81732425f20ab8dfef415b2530b067cba9a
FireStorm <110357751+FireStorlvl@users.noreply.github.com>
2025-03-30 20:10:38 -0400

    Add spot futures examples (#2)
    * - Add futures examples:
     - Funding
     - Long / Short ratio
     - Open Interest
    - Add spot examples:
     - Price & volume
     - OHLCV
     - Trades
    
    * - Increment version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='amberdata_rest',
     packages=find_packages(include=['amberdata_rest', 'amberdata_rest.*']),
-    version='0.2.0',
+    version='0.2.1',
     description='Amberdata.io REST API SDK',
     author='technology@firestorm.capital',
     install_requires=[],
